4a451aa110ff1d27034abe360256dba522d63f65
wjoverink <willemjan.overink@gmail.com>
2018-10-04 13:58:50 +0200

    solved error

--- src/util/processsvg.js
+++ src/util/processsvg.js
@@ -1,4 +1,4 @@
-import randomcolor from 'randomcolor'
+import {processFunctions} from './processFunctions'
 
 const processSVG = (svg) => {
   const children = [...svg.childNodes]
@@ -6,34 +6,14 @@ const processSVG = (svg) => {
     if (child.setAttribute && child.id){
       const idArray = child.id.split("_")
       idArray.forEach((str) => {
-        if (processTYPES[str]){
-          processTYPES[str](child, idArray)
+        if (processFunctions[str]){
+          processFunctions[str](child, idArray)
         }
 
       })
     }
   });
   return svg
-}
-
-const processTYPES = {
-  randomFill: (element, idArray) => {
-    element.setAttribute("style", "fill:" + randomcolor())
-  },
-  stroke :  (element, idArray) => {
-    element.setAttribute('stroke', randomcolor())
-    element.setAttribute('stroke-width','4')
-  },
-  randomFillBlueColors: (element, idArray) => {
-    element.setAttribute("style", "fill:" + randomcolor({
-      luminosity: 'light',
-      hue: 'blue'
-   }))
-  },
-  addText: (element, idArray) => {
-    element.textContent = "Hi random:"  + randomcolor()
-    element.setAttribute("style", "fill:purple")
-  },
 }
 
 export {
