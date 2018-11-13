9787076dbb6a0b942fec47527c84d0d421fd0d1d
wj.overink <Willem-Jan.Overink@syskron.com>
2018-11-13 16:51:15 +0100

    feature: basic collession detection

--- src/util/processFunctions.js
+++ src/util/processFunctions.js
@@ -1,81 +1,93 @@
 import contrast from 'get-contrast'
-import { find, last } from 'lodash'
+import { find, last, first, difference } from 'lodash'
+import shuffle from 'shuffle-array'
 
 const processFunctions = {
-  palleteColor:  (element, idArray, vars) => {
-    const pal =  vars["colorPalette"]
-    var random = Math.floor((Math.random() * pal.length-1) + 1)
-    while (last(vars["usedColors"]) === pal[random] && pal.length>1) {
-      random = Math.floor((Math.random() * pal.length-1) + 1)
+  palleteColor: (element, idArray, vars) => {
+    const pal = vars["colorPalette"]
+    var random = Math.floor((Math.random() * pal.length - 1) + 1)
+    while (last(vars["usedColors"]) === pal[random] && pal.length > 1) {
+      random = Math.floor((Math.random() * pal.length - 1) + 1)
     }
     vars["usedColors"].push(pal[random])
     element.setAttribute("style", "fill:" + pal[random])
-   
+
   },
-  palleteColorNotUsed:  (element, idArray, vars, index) => {
-    const pal =  vars["colorPalette"]
-    
-    if (!vars["autoUsedColors"]){
-      vars["autoUsedColors"]=[]
+  palleteColorNotUsed: (element, idArray, vars, index) => {
+    const pal = shuffle(vars["colorPalette"], { 'copy': true })
+
+    if (!vars["autoUsedColors"]) {
+      vars["autoUsedColors"] = []
     }
-    if (!vars["parentElements"]){
-      vars["parentElements"]=[]
+    if (!vars["parentElements"]) {
+      vars["parentElements"] = []
     }
+
     let parentElement
     //parentElement
-    if (element.parentElement.nodeName === 'g'){
-      parentElement = find(vars["parentElements"], {parent:element.parentElement});
-      if (!parentElement){
-        parentElement = {parent:element.parentElement, color:undefined}
+    if (element.parentElement.nodeName === 'g') {
+      parentElement = find(vars["parentElements"], { parent: element.parentElement });
+      if (!parentElement) {
+        parentElement = { parent: element.parentElement, color: undefined }
         vars["parentElements"].push(parentElement)
       } else {
         element.setAttribute("style", "fill:" + parentElement.color)
         return
       }
     }
-    
-    const lastUColorlength= vars["autoUsedColors"].length
-    if (lastUColorlength===pal.length){
-      vars["autoUsedColors"].length = 0
-    }
-    let random = Math.floor((Math.random() * pal.length-1) + 1)
-    let breakCounter = 0
-    let lastColor = vars["autoUsedColors"][lastUColorlength-1] || "#FFFFFF"
-    while ((vars["autoUsedColors"].includes(pal[random]) || 
-      (contrast.ratio(lastColor, pal[random]) < 2)) &&
-      pal.length>1 && 
-      breakCounter < pal.length*3 &&
-      lastUColorlength<pal.length) {
-        random = Math.floor((Math.random() * pal.length-1) + 1)
-        breakCounter++
-    }
-    let newColor = pal[random]
-    if (breakCounter>=pal.length*3){
-      const pal =  vars["colorPalette"]
-      random = Math.floor((Math.random() * pal.length-1) + 1)
-      let newbreakcounter = 0
-      while (newbreakcounter < pal.length && (vars["autoUsedColors"].includes(pal[random]))) {
-        random = Math.floor((Math.random() * pal.length-1) + 1)
-        newbreakcounter++
+    console.log(element)
+    //[{control:child,color:undefined,enclosed:[], intersection:[]}]
+    const collisionsColors = vars["collisions"]
+    let newColor = undefined
+    const collision = collisionsColors[index]
+    if (collision.enclosed.length === 0 && collision.intersection.length === 0){
+      newColor = first(difference(pal, vars["autoUsedColors"]))
+      if (!newColor){
+        vars["autoUsedColors"] = []
+        newColor = first(difference(pal, [last(vars["usedColors"])]))
       }
-      newColor = pal[random]
-      if (newbreakcounter>=pal.length){
-        newColor = "#FFFFFF"
+    } else {
+      newColor = findBestColorsForCollision(collisionsColors, collision, pal)
+      if (newColor === null){
+        const usedColors = [last(vars["usedColors"])]
+        const autoUsedColors = [...vars["autoUsedColors"]]
+        const encl = find(collisionsColors, {control:last(collision.enclosed)})
+        if (encl && encl.color){
+          usedColors.push(encl.color)
+          autoUsedColors.push(encl.color)
+        }
+        newColor = first(difference(pal, autoUsedColors))
+        if (!newColor){
+          vars["autoUsedColors"] = []
+          newColor = first(difference(pal, usedColors))
+        }
+
       }
+
     }
-    vars["autoUsedColors"].push(newColor)
+    if (!newColor){
+      newColor = "white"
+    }
     vars["usedColors"].push(newColor)
-    if (vars["allChildren"].length-1 === index && !vars["usedColors"].includes(vars["mainColor"])){
-      random = Math.floor((Math.random() * vars["allChildren"].length-1) + 1)
-      vars["allChildren"][random].setAttribute("style", "fill:" + vars["mainColor"])
-    }
-    if (parentElement){
+    vars["autoUsedColors"].push(newColor)
+    collision.color = newColor
+
+    // const colParent = find(vars["collisions"], element)
+    // if (colParent){
+    //   colParent.color = newColor
+    // }
+
+    // if (vars["allChildren"].length-1 === index && !vars["usedColors"].includes(vars["mainColor"])){
+    //   random = Math.floor((Math.random() * vars["allChildren"].length-1) + 1)
+    //   vars["allChildren"][random].setAttribute("style", "fill:" + vars["mainColor"])
+    // }
+    if (parentElement) {
       parentElement.color = newColor
     }
     element.setAttribute("style", "fill:" + newColor)
   },
   isMainColor: (element, idArray, vars) => {
-    vars["mainColor"] =  last(vars["usedColors"])
+    vars["mainColor"] = last(vars["usedColors"])
   },
   mainColor: (element, idArray, vars) => {
     vars["usedColors"].push(vars["mainColor"])
@@ -94,45 +106,85 @@ const processFunctions = {
   lastUsedColor: (element, idArray, vars) => {
     element.setAttribute("style", "fill:" + last(vars["usedColors"]))
   },
-  colorContrast:  (element, idArray, vars) => {
+  colorContrast: (element, idArray, vars) => {
     const newColor = randomColorWithContrast(1.3, last(vars["usedColors"]), vars["colorPalette"])
     element.setAttribute("style", "fill:" + newColor)
     vars["usedColors"].push(newColor)
   },
-  colorContrastMain:  (element, idArray, vars) => {
+  colorContrastMain: (element, idArray, vars) => {
     const newColor = randomColorWithContrast(1.2, vars["mainColor"], vars["colorPalette"])
     element.setAttribute("style", "fill:" + newColor)
     vars["usedColors"].push(newColor)
   },
-  colorContrastMainSecondary:  (element, idArray, vars) => {
-      const pal =  vars["colorPalette"]
-      const posColors = []
-      for (let index = 0; index < pal.length; index++) {
-        var b = pal[index]
-        var c = contrast.ratio(vars["secondaryColor"], b)
-        var e = contrast.ratio(vars["mainColor"], b)
-        if (c>1.2 && e>1.2){
-          posColors.push(b)
-        }
+  colorContrastMainSecondary: (element, idArray, vars) => {
+    const pal = vars["colorPalette"]
+    const posColors = []
+    for (let index = 0; index < pal.length; index++) {
+      var b = pal[index]
+      var c = contrast.ratio(vars["secondaryColor"], b)
+      var e = contrast.ratio(vars["mainColor"], b)
+      if (c > 1.2 && e > 1.2) {
+        posColors.push(b)
       }
-      const random = Math.floor((Math.random() * posColors.length-1) + 1)
-      const newColor = posColors[random]
-      element.setAttribute("style", "fill:" + newColor)
-      vars["usedColors"].push(newColor)
+    }
+    const random = Math.floor((Math.random() * posColors.length - 1) + 1)
+    const newColor = posColors[random]
+    element.setAttribute("style", "fill:" + newColor)
+    vars["usedColors"].push(newColor)
   },
 }
 
-const randomColorWithContrast= (contastRatio, contrastColor, palette) => {
-  const pal =  palette
+const findBestColorsForCollision = (collisions, collision, palette) => {
+  const enclosed =  find(collisions, {control:last(collision.enclosed)})
+  // const intersection =  find(collisions, {control:first(collision.intersection)})
+  let newColor = undefined
+  let hasColor = false
+  let hasEnclosed = false
+  palette.forEach(element => {
+    if (enclosed && enclosed.color){
+      hasEnclosed = true
+      //if (contrast.ratio(enclosed.color, element) > 2){
+      if (contrast.isAccessible(enclosed.color, element)){
+        newColor = element
+        hasColor = true
+      }
+    }
+    if (((!hasEnclosed || (hasEnclosed && hasColor)) && (!enclosed || enclosed.color!==element))){
+      collision.intersection.some(el => {
+        if (!enclosed || el!==enclosed.control){
+          const intersection =  find(collisions, {control:el})
+          if (intersection && intersection.color){
+            hasColor = true
+            if (contrast.ratio(intersection.color, element) > 1.5) {
+              newColor = element 
+            } else {
+              newColor = undefined
+              return true
+            }
+          }
+        }
+        return false
+      })
+    }
+  })
+  if (!hasColor){
+    return null
+  }
+
+  return newColor
+}
+
+const randomColorWithContrast = (contastRatio, contrastColor, palette) => {
+  const pal = palette
   const posColors = []
   for (let index = 0; index < pal.length; index++) {
     var b = pal[index]
     var c = contrast.ratio(contrastColor, b)
-    if (c>contastRatio){
+    if (c > contastRatio) {
       posColors.push(b)
     }
   }
-  const random = Math.floor((Math.random() * posColors.length-1) + 1)
+  const random = Math.floor((Math.random() * posColors.length - 1) + 1)
   return posColors[random]
 }
 
--- src/util/processsvg.js
+++ src/util/processsvg.js
@@ -3,6 +3,7 @@ import { processVars } from './processVars'
 const splitOn  = ";"
 
 const processSVG = (svg, processvars) => {
+  console.log("=============START================")
   const children = recursivelyGetChildNodes(svg)
   processvars["allChildren"] = children
   children.forEach((child, index) => { 
@@ -20,8 +21,49 @@ const processSVG = (svg, processvars) => {
       //AUTO color
       processFunctions["palleteColorNotUsed"](child, [], processvars, index)
     }
-  });
+  })
+  console.log("=============END================")
   return svg
+}
+
+const collisionDetection = (svg) => {
+  const collisions = []
+  const children = recursivelyGetChildNodes(svg)
+  children.forEach((child, index) => { 
+    const coll ={control:child,color:undefined,enclosed:[], intersection:[]}
+    collisions.push(coll)
+    children.forEach((compChild, compIndex) => { 
+      if (child!==compChild){
+        // var box = compChild.getBBox()
+        var checkEnclosure = enclosesRect(child, compChild)
+        var checkIntersection = intersectRect(child,compChild)
+        if (checkEnclosure){
+          coll.enclosed.push(compChild)
+        } else if (checkIntersection && ! enclosesRect(compChild, child)){
+          coll.intersection.push(compChild)
+        }
+      }
+    })
+  })
+  return collisions
+}
+
+
+const enclosesRect = (c1, c2) => {
+  var r1 = c1.getBBox();   
+  var r2 = c2.getBBox();   
+  return r2.x <=r1.x && r2.y <=r1.y && r2.width+r2.x >=r1.width+r1.x && r2.height+r2.y >= r1.height+r1.y
+}
+
+const intersectRect = (c1, c2) => {
+  var r1 = c1.getBoundingClientRect();    //BOUNDING BOX OF THE FIRST OBJECT
+  var r2 = c2.getBoundingClientRect();    //BOUNDING BOX OF THE SECOND OBJECT
+
+  //CHECK IF THE TWO BOUNDING BOXES OVERLAP
+return !(r2.left > r1.right || 
+         r2.right < r1.left || 
+         r2.top > r1.bottom ||
+         r2.bottom < r1.top);
 }
 
 const canBeTarget = (child) => {
@@ -58,5 +100,6 @@ const getVarsFromSVG = (svg) => {
 
 export {
   processSVG,
-  getVarsFromSVG
+  getVarsFromSVG,
+  collisionDetection
 }
